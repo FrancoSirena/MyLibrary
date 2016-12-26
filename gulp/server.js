879fe5ced342a4b42e0aa25ed6a577e85f564966
Franco C. Sirena <fsirena@gmail.com>
2016-12-26 13:47:27 -0200

    Configuring the NPM Start script

--- gulp/server.js
+++ gulp/server.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
-    gls = require('gulp-live-server');
+    gls = require('gulp-live-server'),
+    open = require('gulp-open');
 
 gulp.task('server', function() {
     var sources = [
@@ -8,11 +9,13 @@ gulp.task('server', function() {
 
     var server = gls.static('./',process.env.PORT, process.env.IP);
     server.start();
-
+    gulp.src('./index.html').pipe(open());
     //use gulp.watch to trigger server actions(notify, start or stop)
     return gulp.watch(sources, function (file) {
         server.notify.apply(server, [file]);
     });
+
+
 });
 
 gulp.task('serve', ['watch','server']);
